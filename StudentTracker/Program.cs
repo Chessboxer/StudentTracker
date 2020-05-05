b870f7b478a4e12543243c95e25d801d5ede4047
Chessboxer <mgregor81@gmail.com>
2020-05-05 14:33:47 -0700

    Implement school field & WIP using enum/switch

--- StudentTracker/Program.cs
+++ StudentTracker/Program.cs
@@ -5,11 +5,9 @@ namespace StudentTracker
 {
     class Program 
     {
+        static List<Student> Students = new List<Student>();
         static void Main(string[] args)
-        {
-            
-            var students = new List<Student>();
-
+        { 
             var adding = true;
             while (adding)
             {
@@ -21,13 +19,15 @@ namespace StudentTracker
 
                     newStudent.Grade = util.Console.AskInt(newStudent.Name + "'s Grade: ");
 
+                    newStudent.School = util.Console.AskInt("Enter school corresponding school: \n 0 - Hogwarts \n 1 - Harvard \n 2 - MIT \n");
+
                     newStudent.Birthday = util.Console.Ask(newStudent.Name + "'s Birthday: ");
 
                     newStudent.Address = util.Console.Ask(newStudent.Name + "'s Address: ");
 
                     newStudent.Phone = util.Console.AskInt(newStudent.Name + "'s Phone Number: ");
 
-                    students.Add(newStudent);
+                    Students.Add(newStudent);
                     Student.Count++;
 
                     Console.WriteLine("Student Count:  {0}", Student.Count);
@@ -55,6 +55,27 @@ namespace StudentTracker
                 Console.WriteLine("Name: {0}     | Grade: {1}", student.Name, student.Grade);       
             }
         }
+
+        public void Exports()
+        {
+            foreach (var student in Students)
+            {
+                switch (student.School)
+                {
+                    case 0:
+                        Console.WriteLine("Exporting to Hogwarts");
+                        break;
+                    case 1:
+                        Console.WriteLine("Exporting to MIT");
+                        break;
+                    case 2:
+                        Console.WriteLine("Exporting to Harvard");
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 
     class Member
@@ -72,7 +93,9 @@ namespace StudentTracker
     class Student : Member {
         public static int Count = 0;
         public int Grade;
+        public int School;
         public string Birthday;
+
 
         public Student()
         {
